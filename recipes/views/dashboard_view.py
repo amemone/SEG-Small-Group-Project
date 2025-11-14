6eab7a95b59709a7bb68fb9817967c9ff7150d75
Ana Clara Monteiro <ana.monteiro@kcl.ac.uk>
2025-11-14 00:18:40 +0000

    Dashboard and feed now shows recepies accordingly

--- recipes/views/dashboard_view.py
+++ recipes/views/dashboard_view.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from recipes.models.recipes import Recipe
 
 
-# @login_required    commented out for now
+@login_required
 def dashboard(request):
     """
     Display the current user's dashboard.
@@ -11,7 +12,15 @@ def dashboard(request):
     It ensures that only logged-in users can access the page. If a user
     is not authenticated, they are automatically redirected to the login
     page.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: Rendered dashboard template with user data and their recipes.
     """
 
     current_user = request.user
-    return render(request, 'dashboard.html', {'user': current_user})
+    user_recipes = Recipe.objects.filter(
+        user=current_user).order_by('-publication_date')
+    return render(request, 'dashboard.html', {'user': current_user, 'recipes': user_recipes})
