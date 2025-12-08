18f17d8a61a8b6f6e4e10563139faff046c21711
Nabil Ahmed <nabil.ahmed@kcl.ac.uk>
2025-12-08 14:28:40 +0000

    Add creation of recipes into the test set suite set up

--- recipes/tests/views/test_sort_by_popularity.py
+++ recipes/tests/views/test_sort_by_popularity.py
@@ -28,6 +28,21 @@ class SortByPopuarityTest(TestCase):
             first_name='User',
             last_name='Three',
         )
+        self.recipe1 = Recipe.objects.create(
+            title='Recipe 1',
+            description='desc',
+            user=self.user1,
+        )
+        self.recipe2 = Recipe.objects.create(
+            title='Recipe 2',
+            description='desc',
+            user=self.user1,
+        )
+        self.recipe3 = Recipe.objects.create(
+            title='Recipe 3',
+            description='desc',
+            user=self.user1,
+        )
         self.browse_url = reverse('recipe_browse')
 
     def test_most_favourited_recipe_appears_first_when_sorted_by_popularity(self):
@@ -63,50 +78,23 @@ class SortByPopuarityTest(TestCase):
         self.assertEqual(recipes[2], recipe3)
 
     def test_recipes_with_zero_favourites_are_listed_last(self):
-        recipe1 = Recipe.objects.create(
-            title='Recipe 1',
-            description='desc',
-            user=self.user1,
-        )
-        recipe2 = Recipe.objects.create(
-            title='Recipe 2',
-            description='desc',
-            user=self.user1,
-        )
-        recipe3 = Recipe.objects.create(
-            title='Recipe 3',
-            description='desc',
-            user=self.user1,
-        )
-
-        Favourite.objects.create(user=self.user1, recipe=recipe1)
+        Favourite.objects.create(user=self.user1, recipe=self.recipe1)
 
         response = self.client.get(self.browse_url + '?popular=1')
         self.assertEqual(response.status_code, 200)
 
         recipes = list(response.context['recipes'])
-        self.assertEqual(recipes[0], recipe1)
-        self.assertIn(recipe2, recipes[1:])
-        self.assertIn(recipe3, recipes[1:])
+        self.assertEqual(recipes[0], self.recipe1)
+        self.assertIn(self.recipe2, recipes[1:])
+        self.assertIn(self.recipe3, recipes[1:])
 
     def test_default_ordering_ignores_popularity_when_popular_not_set(self):
-        recipe1 = Recipe.objects.create(
-            title='Recipe 1',
-            description='desc',
-            user=self.user1,
-        )
-        recipe2 = Recipe.objects.create(
-            title='Recipe 2',
-            description='desc',
-            user=self.user1,
-        )
-
-        Favourite.objects.create(user=self.user1, recipe=recipe1)
-        Favourite.objects.create(user=self.user2, recipe=recipe1)
+        Favourite.objects.create(user=self.user1, recipe=self.recipe1)
+        Favourite.objects.create(user=self.user2, recipe=self.recipe1)
 
         response = self.client.get(self.browse_url)
         self.assertEqual(response.status_code, 200)
 
         recipes = list(response.context['recipes'])
-        self.assertIn(recipe1, recipes)
-        self.assertIn(recipe2, recipes)
+        self.assertIn(self.recipe1, recipes)
+        self.assertIn(self.recipe2, recipes)
