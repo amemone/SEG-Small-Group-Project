ae4133bd6a1bd8b985a89c21ef16926e8f55e533
Samuel De Pinheiro <samuel.pinheiro@kcl.ac.uk>
2025-12-10 12:37:36 +0000

    Added user profiles and neatened up own profile

--- recipes/helpers.py
+++ recipes/helpers.py
@@ -1,1 +1,63 @@
 ### Helper function and classes go here.
+from django.core.paginator import Paginator
+from recipes.models.follow import Follow
+from recipes.models.recipes import Recipe
+
+def get_following_count(user):
+    return Follow.objects.filter(follower=user).count()
+
+def get_following_users(user):
+    followings = Follow.objects.filter(follower=user).select_related('followee')
+    return [following_user.followee for following_user in followings]
+
+def get_follower_count(user):
+    return Follow.objects.filter(followee=user).count()
+
+def get_follower_users(user):
+    followers = Follow.objects.filter(followee=user).select_related('follower')
+    return [follower_user.follower for follower_user in followers]
+
+def paginate_following(request,user):
+    followings = get_following_users(user)
+    following_paginate = Paginator(followings, 5)
+    page_number = request.GET.get('following_page')
+    page_object = following_paginate.get_page(page_number)
+    return page_object
+    
+def paginate_followers(request,user):
+    followers = get_follower_users(user)
+    follower_paginate = Paginator(followers, 5)
+    page_number = request.GET.get('follower_page')
+    page_object = follower_paginate.get_page(page_number)
+    return page_object
+
+def user_is_following(viewer, profile_user):
+    return Follow.objects.filter(
+        follower=viewer,
+        followee=profile_user
+    ).exists()
+
+def paginate_favourite_recipes(request, user):
+    favourites_recipes = (
+        Recipe.objects
+        .filter(favourite__user=user)
+        .order_by('-favourite__favourited_at')
+    )
+    favourite_paginate = Paginator(favourites_recipes, 9)
+    page_number = request.GET.get('page')
+    return favourite_paginate.get_page(page_number)
+
+
+def paginate_recipes_user(request, viewer, profile_user):
+    if viewer == profile_user:
+        recipes = Recipe.objects.filter(user=profile_user)
+    else:
+        is_following = user_is_following(viewer, profile_user)
+        if is_following:
+            recipes = Recipe.objects.filter(user=profile_user)
+        else:
+            recipes = Recipe.objects.filter(user=profile_user, visibility="public")
+    recipes = recipes.order_by('-publication_date')
+    recipes_paginate = Paginator(recipes, 9)
+    page_number = request.GET.get('page')
+    return recipes_paginate.get_page(page_number)
---
+++ recipes/views/user_profile_view.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, get_object_or_404
+from recipes.models.user import User
+from recipes.helpers import (
+    paginate_followers,
+    paginate_following,
+    get_following_count,
+    get_follower_count,
+    paginate_recipes_user,
+    user_is_following
+)
+
+def user_profile_view(request, username):
+    profile_user = get_object_or_404(User, username=username)
+    context = {
+        'profile_user': profile_user,
+        'following': get_following_count(profile_user),
+        'user_followings': paginate_following(request, profile_user),
+        'followers': get_follower_count(profile_user),
+        'user_followers': paginate_followers(request, profile_user),
+        'is_following': user_is_following(request.user, profile_user),
+        'recipes': paginate_recipes_user(request, request.user, profile_user),
+    }
+
+    return render(request, 'user_profile.html', context)
