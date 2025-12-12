f28386870a886caecca2c91f49dd3b50e6d1e8bd
Nabil Ahmed <nabil.ahmed@kcl.ac.uk>
2025-12-12 11:26:35 +0000

    Make it so that when nothing has been searched for on the browse user page it shows the top 5 most followed users

--- recipes/tests/views/test_user_browse_view.py
+++ recipes/tests/views/test_user_browse_view.py
@@ -1,6 +1,6 @@
 """Tests of the follow view."""
 from django.test import TestCase
-from recipes.models import User
+from recipes.models import User, Follow
 from django.urls import reverse
 
 class UserBrowseView(TestCase):
@@ -15,6 +15,20 @@ class UserBrowseView(TestCase):
         self.user = User.objects.get(username='@johndoe')
         self.second_user = User.objects.get(username='@janedoe')
         self.third_user = User.objects.get(username='@petrapickles')
+        self.fourth_user = User.objects.create_user(
+            username='@sophieharris',
+            email='sophieharris@email.org',
+            password='Password123',
+            first_name='Sophie',
+            last_name='Harris',
+            )
+        self.fifth_user = User.objects.create_user(
+            username='@frankbegum',
+            email='frankbegum@email.org',
+            password='Password123',
+            first_name='Frank',
+            last_name='Begum'
+            )
         self.url = reverse('user_browse')
 
     def _search_and_get_results(self, search):
@@ -44,4 +58,32 @@ class UserBrowseView(TestCase):
 
     def test_search_multiple_users(self):
         self.assertEqual(self._search_and_get_results('doe'),[self.second_user, self.user])
+
+    def test_get_top_five_most_followed_users(self):
+        Follow.objects.create(follower=self.second_user, followee=self.user)
+        Follow.objects.create(follower=self.third_user, followee=self.user)
+        Follow.objects.create(follower=self.fourth_user, followee=self.user)
+        Follow.objects.create(follower=self.fifth_user, followee=self.user)
+
+        Follow.objects.create(follower=self.user, followee=self.second_user)
+        Follow.objects.create(follower=self.third_user, followee=self.second_user)
+        Follow.objects.create(follower=self.fourth_user, followee=self.second_user)
+        Follow.objects.create(follower=self.fifth_user, followee=self.second_user)
+
+
+        Follow.objects.create(follower=self.user, followee=self.third_user)
+        Follow.objects.create(follower=self.second_user, followee=self.third_user)
+        Follow.objects.create(follower=self.fourth_user, followee=self.third_user)
+
+        Follow.objects.create(follower=self.user, followee=self.fourth_user)
+        Follow.objects.create(follower=self.second_user, followee=self.fourth_user)
+
+        Follow.objects.create(follower=self.user, followee=self.fifth_user)
+
+        response = self.client.get(reverse('user_browse'), {'q': ''})
+        top_users = list(response.context['top_users'])
+        self.assertEqual(len(top_users), 5)
+
+        expected_order = [self.second_user, self.user, self.third_user, self.fourth_user, self.fifth_user]
+        self.assertEqual(top_users, expected_order)
 
--- recipes/views/user_browse_view.py
+++ recipes/views/user_browse_view.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from recipes.models.user import User
+from django.db.models import Count
+from django.core.paginator import Paginator
 
 def user_browse_view(request):
     """
@@ -13,4 +15,21 @@ def user_browse_view(request):
     else:
         users = User.objects.filter(username__icontains=query)
 
-    return render(request, 'user_browse.html', {'users': users})
+    top_users = get_top_following_users(5)
+
+    paginate = Paginator(users, 6)
+    page_number = request.GET.get('page')
+    page_object = paginate.get_page(page_number)
+
+    return render(request, 'user_browse.html', {
+        'users':users,
+        'page_object': page_object,
+        'top_users': top_users,
+        'query':query,
+        })
+
+def get_top_following_users(limit = 5):
+    return (
+        User.objects.annotate(follower_count=Count('followers', distinct=True))
+        .order_by('-follower_count','username')[:limit]
+    )
